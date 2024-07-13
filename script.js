bdc47f90f43f2d38a0bc9f9709032e5c78546fb9
ShahnawazShaikh01 <138504776+ShahnawazShaikh01@users.noreply.github.com>
2024-07-13 08:56:15 +0530

    Add files via upload
    Front-end project using html css  and javascript with many functionalities.

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn=document.getElementById("btn");
+let light=document.getElementById("blink");
+let pic=document.getElementById("pic");
+
+
+function togglebtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+    pic.classList.toggle("picture");
+}
+
+
